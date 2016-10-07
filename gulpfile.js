3e027a4b8b1b0bdc08b49e71f21bbb0e072ea673
ppworks <koshikawa@ppworks.jp>
2016-10-08 01:23:29 +0900

    npm install browser-sync --save-dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 var gulp = require('gulp')
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
+var browser = require("browser-sync");
+
+gulp.task("server", function() {
+  browser({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
 
 gulp.task('sass', function() {
   gulp.src('sass/**/*.scss')
       .pipe(sass())
       .pipe(autoprefixer())
-      .pipe(gulp.dest('./css'));
+      .pipe(gulp.dest('./css'))
+      .pipe(browser.reload({stream:true}));
 });
 
-gulp.task("default", function() {
+gulp.task("default", ["server"], function() {
   gulp.watch("sass/**/*.scss",["sass"]);
 });
